f9981d3564de33344211e7a2f1d57109e825ea08
KevinPerea <70814852+KevinPerea@users.noreply.github.com>
2020-11-28 11:32:17 -0500

    Update agregar_actualizar_tarea.sh

--- agregar_actualizar_tarea.sh
+++ agregar_actualizar_tarea.sh
@@ -1,1 +1,2 @@
 curl -i -X POST -H "Content-Type: application/json" -d '{"id": 56, "nombre":"tarea aaaaaaaaa 2", "fecha_limite": "28/11/2020"}' http://localhost:8080/agregar-actualizar-tarea
+curl -i -X POST -H "Content-Type: application/json" -d '{"id": 34, "nombre":"Trabajo progrmacion", "fecha_limite": "30/11/2020"}'https://psti-web-service-g2.herokuapp.com/
